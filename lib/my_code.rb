8b65859817b35ec363ef43c28a5d47f1dac12756
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-22 06:10:40 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(inputarray)
+  new = []
+  i = 0
+  while i < inputarray.length do
+    new << yield(inputarray[i])
+    i += 1
+  end
+ return new
+end
+
+def reduce(inputarray, startingvalue=nil)
+ if startingvalue
+  
+  output = startingvalue
+  i = 0
+  else
+  output = inputarray[0]
+  i = 1
+ end
+ while i < inputarray.length do
+   output = yield(output, inputarray[i])
+   i += 1
+  end
+ return output
+end
